fc9330cf2cb964a0c861fbed7bc25987cb5f2718
Matheus Hoffmann Silva <hoffmann.matheus@gmail.com>
2017-06-04 16:53:56 +0000

    Using Geocoder to resolve address, with gmaps

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,20 @@
+var NodeGeocoder = require('node-geocoder');
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+var options = {
+  provider: 'google',
+  apiKey: process.env.MAPS_API_KEY
+};
+var geocoder = NodeGeocoder(options);
+
+
+Parse.Cloud.define('resolveAddress', function(req, res) {
+  console.log("request:", req);
+  geocoder.reverse({lat: req.params.lat, lon: req.params.lon}, function(err, result) {
+        console.log(err, result);
+        if (!err && result && result.length > 0) {
+          res.success(result[0]);
+        } else {
+          res.error({err, message: "No location found"});
+        }
+      });
 });
